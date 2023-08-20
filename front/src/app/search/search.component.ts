2faaad97b37497b3d0a4ce90e27b4312e53bb7bd
Dorian Zielinski <dorian.zielinski.pro@gmail.com>
2023-08-20 14:50:43 +0200

    [Front] Update search form using FormBuilder. Bugfix : Clear the page when the search is made with the enter key

--- front/src/app/search/search.component.ts
+++ front/src/app/search/search.component.ts
@@ -1,20 +1,32 @@
 import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 import {MovieSearchService} from "../services/movie-search.service";
 import {Movie} from "../interfaces/movie";
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
 
+  constructor(private formBuilder: FormBuilder) {}
+
   movieSearchService: MovieSearchService = inject(MovieSearchService);
 
+  searchForm = this.formBuilder.group({
+    movieName:''
+  })
+
   movies: Movie[] = [];
+
+  onSubmit(): void {
+    let form = this.searchForm.value;
+    this.searchMovie(<string>form.movieName);
+  }
 
   searchMovie(movie: string) {
     this.movieSearchService.searchMovies(movie)
